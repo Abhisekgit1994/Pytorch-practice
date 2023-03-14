c9fecf74a006d1145a41e51e8e8c51043110c13c
Abhisekgit1994 <a.panigrahi.1994@gmail.com>
2023-03-13 23:36:17 -0400

    simple rnn

--- ConvolutionalNN/simple_cnn.py
+++ ConvolutionalNN/simple_cnn.py
@@ -14,7 +14,7 @@ class CNN(nn.Module):
     def __init__(self, in_channels=1, num_classes=10):
         super(CNN, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=8, kernel_size=(3, 3), padding=(1, 1),
-                               stride=(1, 1))  # same convlutions
+                               stride=(1, 1))  # same convolutions
         self.pool = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
         self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
         self.linear = nn.Linear(16 * 7 * 7, num_classes)
--- tensor fundamentals/reshape.py
+++ tensor fundamentals/reshape.py
@@ -33,3 +33,7 @@ print(x.shape)
 
 print(x.squeeze(1))
 
+x = torch.rand((64,1,28,28))
+print(x.reshape(x.shape[0], x.shape[2],x.shape[3])[0])
+
+
