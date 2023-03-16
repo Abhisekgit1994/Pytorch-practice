8f3e2a09a79440201059e42ddd482153b21cce54
Abhisekgit1994 <a.panigrahi.1994@gmail.com>
2023-03-16 02:38:01 -0400

    Removed output from py file

--- Model from scratch/VGG16/vgg16.py
+++ Model from scratch/VGG16/vgg16.py
@@ -163,20 +163,3 @@ def main():
 if __name__ == '__main__':
     main()
 
-# model progress and performance on CIFAR10 dataset : 1000 classes
-# Epoch [1/1], Step [8/6250], Loss: 26.3157, iteration: 1, time: 0.11(s),
-# Epoch [1/1], Step [16/6250], Loss: 10.3575, iteration: 2, time: 0.11(s),
-# Epoch [1/1], Step [24/6250], Loss: 7.4232, iteration: 3, time: 0.11(s),
-# Epoch [1/1], Step [32/6250], Loss: 2.1376, iteration: 4, time: 0.11(s),
-# .
-# .
-# .
-# Epoch [1/1], Step [6224/6250], Loss: 1.5443, iteration: 778, time: 0.11(s),
-# Epoch [1/1], Step [6232/6250], Loss: 2.0813, iteration: 779, time: 0.11(s),
-# Epoch [1/1], Step [6240/6250], Loss: 2.0809, iteration: 780, time: 0.11(s),
-# Epoch [1/1], Step [6248/6250], Loss: 2.1535, iteration: 781, time: 0.11(s),
-# Epoch [1/1], Step [6250/6250], Loss: 2.1855iteration: 781, time: 0.11(s),
-# Loss at epoch 1 2.198171835727692
-# ******** Printing accuracy for VGG16 model ***********
-# checking accuracy on test data
-# Accuracy is 30.059999465942383
